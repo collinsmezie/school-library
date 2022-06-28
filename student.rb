d5cede72f63263d8a5baf7f44a54fa01b8b6cdb9
collinsmezie <collinschimezieobinwa@gmail.com>
2022-06-28 16:10:39 +0400

    add Person, Student, Teacher classes

---
+++ student.rb
@@ -0,0 +1,22 @@
+require_relative './person'
+
+class Student < Person
+  def initialize(classroom)
+    # super(age, name, parent_permission: parent_permission)
+    super(25, "collins", false)
+
+    @classroom = classroom
+  end
+
+  attr_accessor :classroom, :parent_permission
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+person1 = Student.new(true)
+puts person1.name
+puts person1.age
+puts person1.parent_permission
+puts person1.attr_accessor
