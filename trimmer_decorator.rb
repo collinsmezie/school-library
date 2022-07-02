4a992602099492894f54b1debd5ead02def43d5f
collinsmezie <collinschimezieobinwa@gmail.com>
2022-07-03 02:50:51 +0400

    add command line interface for library

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
 require_relative './base_decorator'
 
 # Trimmer decorator
-class TrimmerDecorator < Base_decorator
+class TrimmerDecorator < BaseDecorator
   def correct_name
     @name = @nameable_obj.correct_name
     @name.length > 10 ? "#{@name[0...10]}..." : @name
